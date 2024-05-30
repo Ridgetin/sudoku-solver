8050420a837a146b0f01777b992820340510f77e
Krish singh <137265866+Ridgetin@users.noreply.github.com>
2024-05-30 07:25:02 +0530

    Create sudoku.py

---
+++ sudoku.py
@@ -0,0 +1,65 @@
+def solve_sudoku(board):
+    if not find_empty_cell(board):
+        return True
+
+    row, col = find_empty_cell(board)
+
+    for num in range(1, 10):
+        if is_valid(board, row, col, num):
+            board[row][col] = num
+
+            if solve_sudoku(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+
+def find_empty_cell(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return i, j
+    return None
+
+
+def is_valid(board, row, col, num):
+    for i in range(9):
+        if board[row][i] == num:
+            return False
+
+    for i in range(9):
+        if board[i][col] == num:
+            return False
+
+    start_row = (row // 3) * 3
+    start_col = (col // 3) * 3
+
+    for i in range(3):
+        for j in range(3):
+            if board[start_row + i][start_col + j] == num:
+                return False
+
+    return True
+
+
+# Example Sudoku board
+board = [
+    [5, 0, 4, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [0, 2, 0, 8, 0, 3, 0, 0, 0],
+    [7, 0, 0, 0, 2, 0, 0, 0, 0],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+if solve_sudoku(board):
+    print("Sudoku solved:")
+    for row in board:
+        print(row)
+else:
+    print("No solution exists for the given Sudoku board.")
